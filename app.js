9d80af95df2a1d06a8f9fa2a6b3ab90366933fd4
quan-p <m.phamquan@gmail.com>
2022-12-15 16:40:34 -0600

    Remove all boilerplate code

--- app.js
+++ app.js
@@ -1,50 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var jwt = require('jsonwebtoken');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use("/", apiRouter);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(5000, () => console.log('Listening on http://localhost:5000/'));
 
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'Welcome to the API'
-  })
-})
-
-app.listen(5000, () => console.log('Server started on port 5000'));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
