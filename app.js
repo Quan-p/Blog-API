f3479efa8f107233d827bab841de0beec0086365
quan-p <m.phamquan@gmail.com>
2022-12-16 17:23:04 -0600

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
-const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
 
 app.use("/", apiRouter);
 
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once('connected', () => {
+  console.log('Database Connected');
+})
 app.listen(5000, () => console.log('Listening on http://localhost:5000/'));
 
 
