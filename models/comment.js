49b5980df2cd988043f8f8d720dbefaa2c9e1e13
quan-p <m.phamquan@gmail.com>
2022-12-20 13:41:25 -0600

    Create comment model

---
+++ models/comment.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema({
+    date: { type: Date, default: Date.now() },
+    text: { type: String, required: true },
+    user: { type: String, required: true }
+})
+
+module.exports = mongoose.model('Comment', CommentSchema);
