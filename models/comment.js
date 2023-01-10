34dfed9b3300485477541496b5bd028122a183eb
quan-p <m.phamquan@gmail.com>
2023-01-10 17:04:13 -0600

    Add associated post IDto comment model

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const CommentSchema = new Schema({
     date: { type: Date, default: Date.now() },
     text: { type: String, required: true },
-    user: { type: String, required: true }
+    user: { type: String, required: true },
+    postId: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Comment', CommentSchema);
