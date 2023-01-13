b60dd4d155754fbf55d2c3b196ca0bd7c7f0fb16
quan-p <m.phamquan@gmail.com>
2023-01-13 16:54:55 -0600

    Create validPassword method to compare passwords and create pre method to use bcrypt to hash password

--- models/user.js
+++ models/user.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     username: {type: String, required: true, maxLength: 20},
     password: {type: String, required: true },
+});
+
+UserSchema.pre('save', async function (next) {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+
+    this.password = hashedPassword;
+    next();
 })
+
+UserSchema.methods.validPassword = async function (password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+};
 
 module.exports = mongoose.model('User', UserSchema);
